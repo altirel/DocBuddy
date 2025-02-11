dba151ed1e62aa56afebefe1ecb07a0064f6445c
Viktor Bakulin <bakulinvm@gmail.com>
2025-02-11 23:33:38 +0700

    Initial commit for buddy-documents service

---
+++ backend/buddy-documents/src/main/java/com/basiliqo/buddy_documents/BuddyDocumentsApplication.java
@@ -0,0 +1,13 @@
+package com.basiliqo.buddy_documents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyDocumentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuddyDocumentsApplication.class, args);
+	}
+
+}
